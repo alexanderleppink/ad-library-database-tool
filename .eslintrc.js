1423bbb2d740244207d1de50d91a06d3b0f7fa12
paulbuehler <paulbuehlerdev@gmail.com>
2024-02-29 16:55:50 +0100

    chore: setup project

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ['next', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['unicorn'],
+  overrides: [
+    // Configuration for TypeScript files
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      plugins: ['@typescript-eslint', 'unused-imports', 'simple-import-sort'],
+      extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ],
+  rules: {
+    // https://stackoverflow.com/a/74327009
+    // Note: you must disable the base rule as it can report incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto'
+      }
+    ],
+    'prefer-const': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error', // Ensure `import type` is used when it's necessary
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true
+        }
+      }
+    ]
+  }
+};
---
+++ types/env.d.ts
@@ -0,0 +1,27 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+      NEXT_PUBLIC_SUPABASE_URL: string;
+      NX_DAEMON: string;
+      TURBO_REMOTE_ONLY: string;
+      TURBO_RUN_SUMMARY: string;
+      VERCEL: string;
+      VERCEL_ENV: string;
+      VERCEL_GIT_COMMIT_AUTHOR_LOGIN: string;
+      VERCEL_GIT_COMMIT_AUTHOR_NAME: string;
+      VERCEL_GIT_COMMIT_MESSAGE: string;
+      VERCEL_GIT_COMMIT_REF: string;
+      VERCEL_GIT_COMMIT_SHA: string;
+      VERCEL_GIT_PREVIOUS_SHA: string;
+      VERCEL_GIT_PROVIDER: string;
+      VERCEL_GIT_PULL_REQUEST_ID: string;
+      VERCEL_GIT_REPO_ID: string;
+      VERCEL_GIT_REPO_OWNER: string;
+      VERCEL_GIT_REPO_SLUG: string;
+      VERCEL_URL: string;
+    }
+  }
+}
+
+export {}

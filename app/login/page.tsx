e0874671bb1027593e5d4c053452fb361f07a3ad
paulbuehler <paulbuehlerdev@gmail.com>
2024-04-13 22:37:47 +0200

    feat: remove sign up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,9 @@
-import { headers } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import { SubmitButton } from './SubmitButton';
 import { TextInput } from 'flowbite-react';
 
-export default function Login({ searchParams }: { searchParams: { message: string } }) {
+export default function Login() {
   const signIn = async (formData: FormData) => {
     'use server';
 
@@ -19,30 +18,6 @@ export default function Login({ searchParams }: { searchParams: { message: strin
 
     if (error) {
       return redirect('/login?message=Could not authenticate user');
-    }
-
-    return redirect('/app');
-  };
-
-  const signUp = async (formData: FormData) => {
-    'use server';
-
-    const origin = headers().get('origin');
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`
-      }
-    });
-
-    if (error) {
-      console.error('Error signing up:', error);
-      return redirect('/login?message=Could not sign up');
     }
 
     return redirect('/app');
@@ -68,18 +43,6 @@ export default function Login({ searchParams }: { searchParams: { message: strin
         >
           Sign In
         </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
       </form>
     </div>
   );

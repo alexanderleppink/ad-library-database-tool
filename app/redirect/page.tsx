5eb548458a1513a7ec8a5fc47e436cedfa4bcd10
paulbuehler <paulbuehlerdev@gmail.com>
2024-04-13 22:01:40 +0200

    test: add auth change event

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useUser } from '@supabase/auth-helpers-react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import type { Database } from '@/types/supabase';
 
 function Page() {
   const user = useUser();
   console.log('user', user);
+  useEffect(() => {
+    const supabase = createClientComponentClient<Database>();
+    supabase.auth.onAuthStateChange((event, session) => {
+      console.log('event', event);
+      console.log('session', session);
+    });
+  }, []);
 
   return <div>Invited!</div>;
 }
